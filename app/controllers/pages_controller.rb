8e875cf8abf0e50dab5e1c7aa312b3766a77cefa
Bence Kiss <kiss.bence.balint@gmail.com>
2017-01-30 00:55:15 -0500

    Added @headers to the controller.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,13 @@
 class PagesController < ApplicationController
   def welcome
-
+    @header = "This is the welcome page header."
   end
   def about
-
+    @header = "I could be the about page."
   end
 
   def contest
-    
+    @header = "Lets see some contest."
   end
 
 end
