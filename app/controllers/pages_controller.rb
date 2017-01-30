503b4e90e137af3f2abc69fee9dec04ecdedc43a
Bence Kiss <kiss.bence.balint@gmail.com>
2017-01-30 01:10:55 -0500

    added kittens and the before_action to the pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 class PagesController < ApplicationController
+  before_action :set_kitten_url, only: [:kitten, :kittens]
+
   def welcome
     @header = "This is the welcome page header."
   end
@@ -10,9 +12,16 @@ class PagesController < ApplicationController
     @header = "Lets see some contest."
   end
   def kitten
+    # since i set it up at before_action i dont need it
+    # set_kitten_url
+  end
+  def kittens
+    # set_kitten_url
+  end
+
+  def set_kitten_url
     requested_size = params[:size]
     @kitten_url = "http://lorempixel.com/#{requested_size}/#{requested_size}/cats"
-
   end
 
 end
