85b5660cc13a1b525ccb2082f8d6e1f07afee7c0
Bence Kiss <kiss.bence.balint@gmail.com>
2017-01-30 01:44:31 -0500

    Refactored routes to paths and _urls

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,7 +10,7 @@ class PagesController < ApplicationController
 
   def contest
     flash[:notice] = "Sorry, the contest has ended."
-    redirect_to "/welcome"
+    redirect_to root_url
   end
   def kitten
     # since i set it up at before_action i dont need it
@@ -21,18 +21,26 @@ class PagesController < ApplicationController
   end
 
   def secrets
+
     if params[:magic_word] == "shh"
-      
+
     else
-      flash[:notice] = "No secret for you."
-      redirect_to '/kittens/500'
+
+      flash[:alert] = "No secret for you."
+      redirect_to kittens_path(500)
     end
   end
 
 
 
   def set_kitten_url
-    requested_size = params[:size]
+    if params[:size]
+      requested_size = params[:size]
+    else
+      params[:size] = 500
+      requested_size = params[:size]
+    end
+
     @kitten_url = "http://lorempixel.com/#{requested_size}/#{requested_size}/cats"
   end
 
